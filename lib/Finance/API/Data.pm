f4db0ebeab5b2bc947d618dd08841d6965656aa1
João Costa <joaocosta@zonalivre.org>
2016-10-01 22:33:09 +0000

    Rename endpoint from parse to indicator. Return more details to help understand results.

--- lib/Finance/API/Data.pm
+++ lib/Finance/API/Data.pm
@@ -8,7 +8,7 @@ use Finance::HostedTrader::Datasource;
 use Finance::HostedTrader::ExpressionParser;
 use Date::Manip;
 
-get '/parse' => sub {
+get '/indicator' => sub {
     my $db = Finance::HostedTrader::Datasource->new();
     my $cfg = $db->cfg;
     my $signal_processor = Finance::HostedTrader::ExpressionParser->new($db);
@@ -23,23 +23,30 @@ get '/parse' => sub {
     content_type 'application/json';
 
     my %results;
-    foreach my $symbol (@{$symbols}) {
-        my $data = $signal_processor->getIndicatorData({
-                                    'fields' => $expr,
-                                    'symbol' => $symbol,
-                                    'tf'     => $timeframe,
-                                    'maxLoadedItems' => $max_loaded_items,
-                                    'numItems' => $max_display_items,
-                                });
-        next unless(defined($data));
-        $results{$symbol} = $data;
-    }
+    my $params = {
+        'fields' => $expr,
+        'tf'     => $timeframe,
+        'maxLoadedItems' => $max_loaded_items,
+        'numItems' => $max_display_items,
+    };
 
+    foreach my $symbol (@{$symbols}) {
+        $params->{symbol} = $symbol;
+        my $indicator_result = $signal_processor->getIndicatorData($params);
+        next unless(defined($indicator_result->{data}));
+        $results{$symbol} = $indicator_result;
+    }
+    delete $params->{symbol};
+
+    my $return_obj = {
+        params => $params,
+        results => \%results,
+    };
 
     if ($jsonp_callback) {
-        return $jsonp_callback . '(' . to_json(\%results) . ')';
+        return $jsonp_callback . '(' . to_json($return_obj) . ')';
     } else {
-        return to_json(\%results);
+        return to_json($return_obj);
     }
 
 };
@@ -76,6 +83,7 @@ get '/signal' => sub {
         next unless(defined($data));
         $results{$symbol} = $data;
     }
+    delete $params->{symbol};
 
     my $return_obj = {
         params => $params,
