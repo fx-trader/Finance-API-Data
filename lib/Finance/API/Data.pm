0be6762b5f0e87eb6f9235fb6dc8afd0706ed36b
João Costa <joaocosta@zonalivre.org>
2016-10-15 22:48:54 +0000

    Adjust to new api that computes all symbol signals in one single database query

--- lib/Finance/API/Data.pm
+++ lib/Finance/API/Data.pm
@@ -171,26 +171,28 @@ get '/signals' => sub {
             status 400;
             return _generate_response( id => "invalid_instrument", message => "instrument $instrument is not supported", url => "http://apidocs.fxhistoricaldata.com/#available-markets" );
         }
-        $params->{symbol} = $instrument;
-        my $signal_result;
-        eval {
-            $signal_result = $signal_processor->getSignalData($params);
-            1;
-        } || do {
-            my $e = $@;
-            status 500;
-
-            if ( $e =~ /Syntax error/ ) {
-                return _generate_response( id => "syntax_error", message => "Syntax error in expression '$expr'", url => "http://apidocs.fxhistoricaldata.com/#signals" );
-            } elsif ( $e =~ /In a multiple timeframe signal expression/ ) {
-                return _generate_response( id => "multiple_timeframe_boolean_operators", message => "In a multiple timeframe signal expression, all boolean operators between timeframe functions need to be the same. This is a limitation of the API.", url => "http://apidocs.fxhistoricaldata.com/#multiple-timeframe-signals" );
-            } else {
-                return _generate_response( id => "internal_error", message => $e, url => "" );
-            }
-        };
-        $results{$instrument} = $signal_result;
     }
-    delete $params->{symbol};
+
+    $params->{instruments} = join(",", @$instruments);
+    my $signal_result;
+    eval {
+        $signal_result = $signal_processor->getSignalData($params);
+        1;
+    } || do {
+        my $e = $@;
+        status 500;
+
+        if ( $e =~ /Syntax error/ ) {
+            return _generate_response( id => "syntax_error", message => "Syntax error in expression '$expr'", url => "http://apidocs.fxhistoricaldata.com/#signals" );
+        } elsif ( $e =~ /In a multiple timeframe signal expression/ ) {
+            return _generate_response( id => "multiple_timeframe_boolean_operators", message => "In a multiple timeframe signal expression, all boolean operators between timeframe functions need to be the same. This is a limitation of the API.", url => "http://apidocs.fxhistoricaldata.com/#multiple-timeframe-signals" );
+        } else {
+            return _generate_response( id => "internal_error", message => $e, url => "" );
+        }
+    };
+    foreach my $instrument (sort keys(%$signal_result)) {
+        $results{$instrument} = $signal_result->{$instrument};
+    }
 
     my %return_obj = (
         params => $params,
